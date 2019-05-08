aaaeb6f71dde00a0b9d62a6375152d5c361abea2
YuXie <richard9596@gmail.com>
2019-05-08 11:37:21 +0800

    add code to build model based on worm reversal response

--- custom/read_connection.py
+++ custom/read_connection.py
@@ -1,3 +1,29 @@
-'''
+"""
 Read worm connectome from csv file and generate connectivity constrain.
-'''
+
+Neuron indices:
+Sensory:
+ALML 7, ALMR 8
+AVM 26
+
+Forward:
+AVBL 80, AVBR 81
+RIBL 99, RIBR 100
+AIYL 74, AIYR 75
+
+Reversal:
+AIBL 70, AIBR 71
+AVAL 78, AVAR 79
+RIML 106, RIMR 107
+
+Turn:
+RIVL 117, RIVR 118
+SMDDL 145, SMDDR 146, SMDVL 146, SMDVR 148
+
+"""
+
+def neuron_idx(neuron_name=None):
+    neuron_name
+    idx
+    return idx
+
