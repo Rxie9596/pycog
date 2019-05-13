a1213ad4a0cdc272782b5d6b8c5359606109d663
YuXie <richard9596@gmail.com>
2019-05-13 11:43:29 +0800

    add function to plot activate state

--- custom/analysis/ana_reversal.py
+++ custom/analysis/ana_reversal.py
@@ -76,5 +76,117 @@ def do(action, args, p):
 
     #-------------------------------------------------------------------------------------
 
+    # =========================================================================================
+    # Active state example
+    # =========================================================================================
+
+    elif action == 'activatestate':
+        import numpy as np
+
+        from pycog import RNN
+        from pycog.figtools import Figure
+
+        # Model
+        m = p['model']
+
+        # Intensity
+        try:
+            intensity = float(args[0])
+        except:
+            intensity = 1
+
+        # Create RNN
+        if 'init' in args:
+            print("* Initial network.")
+            base, ext = os.path.splitext(p['savefile'])
+            savefile_init = base + '_init' + ext
+            rnn = RNN(savefile_init, {'dt': p['dt']}, verbose=True)
+        else:
+            rnn = RNN(p['savefile'], {'dt': p['dt']}, verbose=True)
+
+        trial_func = p['model'].generate_trial
+        trial_args = {
+            'name': 'test',
+            'catch': False,
+            'intensity': intensity,
+        }
+        info = rnn.run(inputs=(trial_func, trial_args), seed=p['seed'])
+
+        # Summary
+        mean = np.mean(rnn.z)
+        std = np.std(rnn.z)
+        print("Intensity: {:.6f}".format(intensity))
+        print("Mean output: {:.6f}".format(mean))
+        print("Std. output: {:.6f}".format(std))
+
+        # Figure setup
+        x = 0.12
+        y = 0.12
+        w = 0.80
+        h = 0.80
+        dashes = [3.5, 1.5]
+
+        t_forward  = 1e-3 * np.array(info['epochs']['forward']);
+        t_stimulus = 1e-3 * np.array(info['epochs']['stimulus']);
+        t_reversal = 1e-3 * np.array(info['epochs']['reversal']);
+
+        fig = Figure(w=4, h=3, axislabelsize=7, labelpadx=5, labelpady=5, thickness=0.6,
+                     ticksize=3, ticklabelsize=6, ticklabelpad=2)
+        plots = {
+            'in': fig.add([x, y + 0.72 * h, w, 0.3 * h]),
+            'out': fig.add([x, y, w, 0.65 * h]),
+            }
+
+        plot = plots['in']
+        plot.ylabel('Input', labelpad=7, fontsize=6.5)
+
+        plot = plots['out']
+        plot.xlabel('Time (sec)', labelpad=6.5)
+        plot.ylabel('Output', labelpad=7, fontsize=6.5)
+
+        # -----------------------------------------------------------------------------------------
+        # Input
+        # -----------------------------------------------------------------------------------------
+
+        plot = plots['in']
+        plot.axis_off('bottom')
+
+        plot.plot(1e-3 * rnn.t, rnn.u[0], color=Figure.colors('red'), lw=0.5)
+
+        # -----------------------------------------------------------------------------------------
+        # Output
+        # -----------------------------------------------------------------------------------------
+
+        plot = plots['out']
+
+        # Outputs
+        colors = [Figure.colors('orange'), Figure.colors('blue')]
+        plot.plot(1e-3 * rnn.t, rnn.z[0], color=colors[0], label='Forward module')
+        plot.plot(1e-3 * rnn.t, rnn.z[1], color=colors[1], label='Reversal module')
+        plot.xlim(1e-3 * rnn.t[0], 1e-3 * rnn.t[-1])
+        plot.lim('y', np.ravel(rnn.z), lower=0)
+
+        # Legend
+        props = {'prop': {'size': 7}}
+        plot.legend(bbox_to_anchor=(1.1, 1.6), **props)
+
+
+        plot.vline(t_forward[-1], color='0.2', linestyle='--', lw=1, dashes=dashes)
+        plot.vline(t_reversal[0], color='0.2', linestyle='--', lw=1, dashes=dashes)
+
+        # Epochs
+        plot.text(np.mean(t_forward), plot.get_ylim()[1], 'forward',
+                  ha='center', va='center', fontsize=7)
+        plot.text(np.mean(t_stimulus), plot.get_ylim()[1], 'stimulus',
+                  ha='center', va='center', fontsize=7)
+        plot.text(np.mean(t_reversal), plot.get_ylim()[1], 'reversal',
+                  ha='center', va='center', fontsize=7)
+
+        if 'init' in args:
+            fig.save(path=p['figspath'], name=p['name'] + '_' + action + '_init')
+        else:
+            fig.save(path=p['figspath'], name=p['name'] + '_' + action)
+        fig.close()
+
     else:
         print("[ {}.do ] Unrecognized action.".format(THIS))
