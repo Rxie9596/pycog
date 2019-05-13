2c1b16c463d0247d980d19fd0931042266055b3a
YuXie <richard9596@gmail.com>
2019-05-13 18:50:45 +0800

    correct nconditions

--- custom/models/worm_reversal.py
+++ custom/models/worm_reversal.py
@@ -17,8 +17,8 @@ Nout = 2
 # -----------------------------------------------------------------------------------------
 
 intensity_range  = [1]
-nconditions      = 5*len(intensity_range)
-pcatch           = 1/(nconditions + 1)
+nconditions      = len(intensity_range)
+pcatch           = 1/(5*nconditions + 1)
 
 def generate_trial(rng, dt, params):
     # -------------------------------------------------------------------------------------
